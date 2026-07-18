a34ef6e4f19f41fd664e5e2c204317de9de714b7
igord <igordf2502@gmail.com>
2026-07-18 18:01:36 -0300

    Estrutura spring

---
+++ src/main/java/com/igor/workshopspringbootjpaapi/WorkshopSpringbootJpaApiApplication.java
@@ -0,0 +1,13 @@
+package com.igor.workshopspringbootjpaapi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WorkshopSpringbootJpaApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(WorkshopSpringbootJpaApiApplication.class, args);
+	}
+
+}
