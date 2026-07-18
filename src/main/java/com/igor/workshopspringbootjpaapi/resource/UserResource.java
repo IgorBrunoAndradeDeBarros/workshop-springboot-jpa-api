cebd7d3c19c6a2a9f2f46cca5bd09e7a9866c125
igord <igordf2502@gmail.com>
2026-07-18 19:32:12 -0300

    Entidade User e seu resource

---
+++ src/main/java/com/igor/workshopspringbootjpaapi/resource/UserResource.java
@@ -0,0 +1,19 @@
+package com.igor.workshopspringbootjpaapi.resource;
+
+import com.igor.workshopspringbootjpaapi.entities.User;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/users")
+public class UserResource {
+
+    @GetMapping
+    public ResponseEntity<User> findAll() {
+        User user = new User(1L, "yan", "yan@gmail.com", "9999999999", "12345");
+        return ResponseEntity.ok(user);
+    }
+
+}
