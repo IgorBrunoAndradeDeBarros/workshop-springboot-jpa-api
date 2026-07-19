7997eebd373193ead3d8deae56e4864f8e1b82b8
igord <igordf2502@gmail.com>
2026-07-19 17:53:41 -0300

    Banco de dados H2, test profile, JPA

--- src/main/java/com/igor/workshopspringbootjpaapi/entities/User.java
+++ src/main/java/com/igor/workshopspringbootjpaapi/entities/User.java
@@ -1,7 +1,9 @@
 package com.igor.workshopspringbootjpaapi.entities;
 
+import jakarta.persistence.*;
 import lombok.*;
 
+import java.awt.geom.GeneralPath;
 import java.io.Serializable;
 
 @AllArgsConstructor
@@ -9,10 +11,14 @@ import java.io.Serializable;
 @Getter
 @Setter
 @EqualsAndHashCode
+@Entity
+@Table(name = "tb_user")
 public class User implements Serializable {
 
     private static final long serialVersionUID = 1L;
 
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
     private String name;
     private String email;
